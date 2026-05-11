d8f8f998935c6591bdf0c583e4e873043c427fb3
Steffi Panakal <steffigeorge222@gmail.com>
2026-05-11 11:44:41 -0400

    Load secret key from environment variable

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import requests
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'orbital_secret_key' # Required for flashing messages
+app.secret_key = os.getenv("SECRET_KEY") # Required for flashing messages
 
 # Setup SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///iss_logs.db'
